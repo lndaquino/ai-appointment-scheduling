9c74959b046af13178cc53fe59bf222759e3c753
Raul Effting <rauleffting94@gmail.com>
2024-01-20 13:21:16 -0300

    feat: add automatic scroll after new message

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -3,11 +3,25 @@ import useAssistant from '@/hooks/useAssistant'
 import { Message } from '@/types'
 import { PaperPlaneRight } from '@phosphor-icons/react'
 import { format } from 'date-fns'
+import React, { useRef, useEffect } from 'react'
 
 export function Chat() {
   const mistralModel = useAiModel('mistral')
   const { messages, input, handleSubmit, handleInputChange } =
     useAssistant(mistralModel)
+
+  const messagesContainerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const scrollToBottom = () => {
+      if (messagesContainerRef.current) {
+        messagesContainerRef.current.scrollTop =
+          messagesContainerRef.current.scrollHeight
+      }
+    }
+
+    scrollToBottom()
+  }, [messages])
 
   function extractHours(date: Date) {
     return format(date, 'HH:mm')
@@ -15,7 +29,10 @@ export function Chat() {
 
   return (
     <div className="">
-      <div className="mt-20 flex h-[calc(100vh-141px)] w-full flex-col overflow-y-auto bg-gray-50 p-4">
+      <div
+        ref={messagesContainerRef}
+        className="mt-20 flex h-[calc(100vh-141px)] w-full flex-col overflow-y-auto bg-gray-50 p-4"
+      >
         {messages.map((message) => (
           <div
             key={message.id}
