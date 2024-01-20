e2e6d9c89040b0704dd7afdb96fb51b898ee31f0
lndaquino <lucas.aquino@gmail.com>
2024-01-19 21:50:29 -0300

    feat: add assistant and aimodel hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 'use client'
-import { Fragment } from 'react'
 import './page.module.css'
+import { Fragment } from 'react'
 import {Header, Chat, Calendar} from '../components'
 import useCalendar from '@/hooks/useCalendar'
+import useAiModel from '@/hooks/useAiModel'
+import useAssistant from '@/hooks/useAssistant'
 
 export default function Home() {
   const {calendar} = useCalendar()
+  const mistralModel = useAiModel('mistral')
+  const {messages, input,handleSubmit,handleInputChange} = useAssistant(mistralModel)
   return (
     <Fragment>
       <Header/>
       <main>
-        <Chat/>
+        <Chat
+          messages={messages}
+        />
         <Calendar
           calendar={calendar}
         />
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
       </main>
     </Fragment>
   )
