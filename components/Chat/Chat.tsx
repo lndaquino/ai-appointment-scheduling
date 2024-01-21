3dde5235ab7cbed88137830333f5d34f7c1c549c
Raul Effting <rauleffting94@gmail.com>
2024-01-20 22:17:20 -0300

    style: change chat font size and resize input

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -29,15 +29,15 @@ export function Chat({ chatAssistant }: IProps) {
     <div className="">
       <div
         ref={messagesContainerRef}
-        className="mt-20 flex h-[calc(100vh-141px)] w-full flex-col overflow-y-auto bg-gray-50 p-4"
+        className="mt-20 flex h-[calc(100vh-188px)] w-full flex-col overflow-y-auto bg-gray-50 p-4"
       >
         {messages.map((message) => (
           <div
             key={message.id}
             className={`mx-2 my-1 flex max-w-xs flex-col rounded shadow ${message.role === 'user' ? 'bubble right bg-indigo-200' : 'bubble left bg-gray-200'}`}
           >
-            <p className="p-2">{message.content}</p>
-            <span className="pb-2 pr-4 text-end text-xs text-gray-600">
+            <p className="p-1 text-sm">{message.content}</p>
+            <span className="pb-1 pr-2 text-end text-xs text-gray-600">
               {extractHours(message.createdAt)}
             </span>
           </div>
@@ -45,10 +45,10 @@ export function Chat({ chatAssistant }: IProps) {
       </div>
       <form
         onSubmit={handleSubmit}
-        className="z-10 flex gap-2 border bg-white p-2"
+        className="z-10 flex gap-2 border-t bg-white px-4 py-8"
       >
         <input
-          className="w-full rounded border border-gray-300 p-2 shadow-xl outline-1 outline-indigo-700"
+          className="w-full rounded border border-gray-300 p-2 text-sm shadow-xl outline-1 outline-indigo-700"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
