9405b1d287225177cc9031ede6c27bc468eb990e
Raul Effting <rauleffting94@gmail.com>
2024-01-27 23:01:50 -0300

    style: improve WelcomeModal responsiveness

--- components/WelcomeModal/WelcomeModal.tsx
+++ components/WelcomeModal/WelcomeModal.tsx
@@ -3,7 +3,11 @@ import { X } from '@phosphor-icons/react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { useEffect, useState } from 'react'
 
-export function WelcomeModal({ handleClose }: { handleClose: () => void }) {
+export function WelcomeModal({
+  handleWelcomeClose,
+}: {
+  handleWelcomeClose: () => void
+}) {
   const [isMounted, setIsMounted] = useState(false)
 
   useEffect(() => {
@@ -15,10 +19,10 @@ export function WelcomeModal({ handleClose }: { handleClose: () => void }) {
       {isMounted && (
         <Dialog.Content
           onOpenAutoFocus={(event) => event.preventDefault()}
-          className="fixed left-1/2 top-1/2 z-50 flex w-80 translate-x-[-50%] translate-y-[-50%] transform flex-col rounded-lg border border-indigo-500 bg-white outline-none xl:mx-8"
+          className="fixed left-1/2 top-1/2 z-50 flex w-80 translate-x-[-50%] translate-y-[-50%] transform flex-col rounded-lg border border-indigo-500 bg-white outline-none md:w-96 xl:mx-8"
         >
           <Dialog.Close
-            onClick={handleClose}
+            onClick={handleWelcomeClose}
             className="flex justify-end p-2 text-yellow-500 hover:opacity-80"
           >
             <X size={21} />
@@ -28,14 +32,14 @@ export function WelcomeModal({ handleClose }: { handleClose: () => void }) {
             <h1 className="mb-2 text-center text-lg font-bold text-indigo-600 md:text-2xl">
               Welcome to the new era of scheduling!
             </h1>
-            <p className="md:text-md mb-4 text-center text-sm text-gray-600">
+            <p className="mb-4 text-center text-sm text-gray-600 md:text-base">
               Discover the power of our state-of-the-art AI virtual assistant,
               meticulously designed to optimize your medical appointment
               scheduling. This intuitive service enables you to effortlessly
               browse and secure appointments, transforming a once-tedious task
               into a simple, click-driven process.
             </p>
-            <p className="md:text-md text-center text-sm text-gray-600">
+            <p className="text-center text-sm text-gray-600 md:text-base">
               Just type in and schedule your appointment. You can also look at
               the calendar below and find out what times are available.
             </p>
