6204fdb062451e3b506b3ba6a1656eb06905602f
Raul Effting <rauleffting94@gmail.com>
2024-01-27 22:41:52 -0300

    style: make welcome modal responsive

--- components/WelcomeModal/WelcomeModal.tsx
+++ components/WelcomeModal/WelcomeModal.tsx
@@ -15,7 +15,7 @@ export function WelcomeModal({ handleClose }: { handleClose: () => void }) {
       {isMounted && (
         <Dialog.Content
           onOpenAutoFocus={(event) => event.preventDefault()}
-          className="fixed left-1/2 top-1/2 z-50 flex w-96 translate-x-[-50%] translate-y-[-50%] transform flex-col rounded-lg border border-indigo-500 bg-white outline-none xl:mx-8"
+          className="fixed left-1/2 top-1/2 z-50 flex w-80 translate-x-[-50%] translate-y-[-50%] transform flex-col rounded-lg border border-indigo-500 bg-white outline-none xl:mx-8"
         >
           <Dialog.Close
             onClick={handleClose}
@@ -25,17 +25,17 @@ export function WelcomeModal({ handleClose }: { handleClose: () => void }) {
           </Dialog.Close>
 
           <div className="flex flex-col items-center justify-center gap-6 p-4 pt-0">
-            <h1 className="mb-2 text-center text-2xl font-bold text-indigo-600">
+            <h1 className="mb-2 text-center text-lg font-bold text-indigo-600 md:text-2xl">
               Welcome to the new era of scheduling!
             </h1>
-            <p className="mb-4 text-center text-gray-600">
+            <p className="md:text-md mb-4 text-center text-sm text-gray-600">
               Discover the power of our state-of-the-art AI virtual assistant,
               meticulously designed to optimize your medical appointment
               scheduling. This intuitive service enables you to effortlessly
               browse and secure appointments, transforming a once-tedious task
               into a simple, click-driven process.
             </p>
-            <p className="text-center text-gray-600">
+            <p className="md:text-md text-center text-sm text-gray-600">
               Just type in and schedule your appointment. You can also look at
               the calendar below and find out what times are available.
             </p>
